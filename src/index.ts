77d3dbf66b8fdb2d3cf9b38de6cf9501a32b1ab8
Msuf123 <akshatmalik18t@gmail.com>
2024-04-19 17:17:52 +0530

    Understood get request

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express,{Express,Response,Request, NextFunction} from 'express'
+import cors from 'cors'
 const app:Express=express()
 const friends=[{name:'abhi',place:'up'},{name:'vardan',place:'up'}]
+app.use(cors({origin:'*'}))
 app.get('/friends',(req:Request,res:Response,next:NextFunction)=>{
+    console.log('Incomming request',req.headers)
     res.send(friends)
 })
-app.listen(3003,()=>{console.log('Server up and running at http://localhost:300')})
+app.listen(3003,()=>{console.log('Server up and running at http://localhost:3003')})
