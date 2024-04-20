452d760822ed92e5167e2c18354e749df2395eae
Msuf123 <akshatmalik18t@gmail.com>
2024-04-20 14:15:12 +0530

    Made server available for reading data

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
 import express,{Express,Response,Request, NextFunction, request} from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 const app:Express=express()
 let friends=[{name:'abhi',place:'up'},{name:'vardan',place:'up'}]
 app.use(cors({origin:'*'}))
-//mY net was not workig so i made body parser from scratch
-app.use((req,res,next)=>{
-    let datareq:Buffer
-    req.on('data',(data)=>{
-        console.log(Buffer.isBuffer(data))
-        datareq=data
-    })
-    req.on('end',()=>{
-        req.body=JSON.parse(datareq.toString())
-        next()
-    })
-    
-    
-    console.log(req.header,req.headers)
-})
+app.use(bodyParser.json())
 app.get('/',(req:Request,res:Response,next:NextFunction)=>{
     res.send('i am up and running can perform further operations')
 })
