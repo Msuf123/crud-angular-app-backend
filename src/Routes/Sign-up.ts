3a899b6ce2dd5350c95d0c426ca78ba355c48896
Msuf123 <akshatmalik18t@gmail.com>
2024-04-29 20:27:11 +0530

    Started my work on token authetication

--- src/Routes/Sign-up.ts
+++ src/Routes/Sign-up.ts
@@ -1,11 +1,20 @@
-import  express, { NextFunction } from "express";
+import  express, { NextFunction, request } from "express";
+import jwt from 'jsonwebtoken'
 import { Request,Response } from "express";
 const signUp=express.Router()
 signUp.post('/',(req:Request,res:Response,next:NextFunction)=>{
    console.log(req.body)
 })
 signUp.post('/verifyUserName',(req:Request,res:Response,next:NextFunction)=>{
-   console.log('in comming usernmae')
-   res.send('valid')
+   console.log(req.body.username)
+   if(req.body.username==='akshatmalik18t@gmail.com'){
+   res.send('valid')}
+   else{
+      res.send('invalid')
+   }
+})
+signUp.post('/user',(req:Request,res:Response,next:NextFunction)=>{
+   const token=jwt.sign({username:'akshatmalik18t@gmailcom'},'shhhh')
+   res.send(token)
 })
 export {signUp}
