bc8f53b0e9ac8e98c28a39c6a18625a91f48780a
Msuf123 <akshatmalik18t@gmail.com>
2024-05-02 19:05:24 +0530

    I hate when my .gitigonre dont work

--- src/Connections/con.ts
+++ src/Connections/con.ts
@@ -1,13 +1,5 @@
 import sql from 'mysql2'
 const con=sql.createPool({
-    host:'localhost',
-    user:'admin',
-    password:'admin@123',
-    waitForConnections:true,
-    connectionLimit:10,
-    connectTimeout:3000,
-    idleTimeout:2000,
-    maxIdle:4,
-    database:'friends'
+   
 })
 export {con}
